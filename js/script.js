79841208d7baca00e184665c903ef92aef8ced28
MarcoAurelioDeFelicis <marcodefelicis24@gmail.com>
2024-04-09 21:16:27 +0200

    ADD: piano progetto js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,36 @@
 console.log ('prova')
+// dichiarazione variabbili prezzi lavoro per ora, dichiarazione ore di lavoro fisse, 
+// creazione array di codici promozionali
+// dichiarazione valore scontro codice promozionale 
+// grab del bottone da html
+// validazione form
+
+// calcolare prezzo 
+// SE 
+// lavoro è backend
+// ALLORA 
+// risultato = backend * orario fisso 
+
+// ALTRIMENTI SE 
+// lavoro è frontend
+// ALLORA 
+// risultato = frontend * orario fisso
+
+// ALTRIMENTI SE 
+// lavoro è projectanalysis
+// ALLORA 
+// risultato = proj + orario fisso 
+
+// calcolare prezzo finale con sconto
+// SE 
+// codicde inserito è presente nell'array sconti 
+// ALLORA 
+// prezzofinale = risultato - 25%
+
+// ALTRIMENTI
+// prezzofinale === risultato
+
+// trasportare il calcolo prezzo in una funzione
+
+// rendere il prezzo finale a numero intero
+
