77f7d7deea1c377c916a03728d3c71fe1284a8ae
MarcoAurelioDeFelicis <158585743+MarcoAurelioDeFelicis@users.noreply.github.com>
2024-04-27 18:53:09 +0200

    ADD: contenuto progetto js : -dichiarazioni variabili -validazione form -creazione funzione per il calcolo del prezzo -formattazione prezzo in un  numero imntero e stampa del prezzo in html

--- js/script.js
+++ js/script.js
@@ -5,46 +5,107 @@ const backend = 20.50
 const frontend = 15.30
 const projAna = 33.60
 
-const oredilavoro =13
+const tipodiLavoro = document.getElementById('kindJob')
+const orariofisso = 13
 
 
 // creazione array di codici promozionali
+const pcode = document.getElementById('code')
 const codiciPromozionali = ['WEB2024']
 
-// dichiarazione valore scontro codice promozionale 
-const sconto = 0.25
 
-// grab del bottone da html
-const button = document.getElementById('form-addingPreventivo')
+// dichiarazione valore scontro codice promozionale 
+const valoresconto = 0.25
+
+// button
+const button = document.getElementById('form-adding-preventivo')
 
 // validazione form
+const forms = document.querySelectorAll('.needs-validation')
 
-// calcolare prezzo 
-// SE 
-// lavoro è backend
-// ALLORA 
-// risultato = backend * orario fisso 
+    Array.from(forms).forEach(form => {
+      form.addEventListener('submit', event => {
+        event.preventDefault();
+        if (!form.checkValidity()) {
+          event.stopPropagation()
+        } else {
+            calcoloPrezzo();
+        }
 
-// ALTRIMENTI SE 
-// lavoro è frontend
-// ALLORA 
-// risultato = frontend * orario fisso
+        form.classList.add('was-validated')
+      }, false)
+    })
 
-// ALTRIMENTI SE 
-// lavoro è projectanalysis
-// ALLORA 
-// risultato = proj + orario fisso 
 
-// calcolare prezzo finale con sconto
-// SE 
-// codicde inserito è presente nell'array sconti 
-// ALLORA 
-// prezzofinale = risultato - 25%
-
-// ALTRIMENTI
-// prezzofinale === risultato
 
 // trasportare il calcolo prezzo in una funzione
+// calcolare prezzo 
+function calcoloPrezzo() {
 
-// rendere il prezzo finale a numero intero
+    const userlavoro = tipodiLavoro.value
+    console.log (userlavoro)
+    // SE
+    // lavoro è backend
+    let totale= 0
+    if (userlavoro === 'backend') {
+        totale = backend * orariofisso
+        
+        // ALTRIMENTI SE 
+        // lavoro è frontend
+    } else if (userlavoro === 'frontend') {
+        totale = frontend * orariofisso
+
+        // ALTRIMENTI SE 
+        // lavoro è projectanalysis
+    } else if (userlavoro === 'projAna') {
+        totale = projAna * orariofisso
+
+    } else if (userlavoro === '') {
+        alert("NON VALIDO")
+    }
+
+    const totalesenzasconti = totale
+    console.log (totalesenzasconti)
+
+
+    // calcolare prezzo finale con sconto
+    const sconto = pcode.value
+    const codiceSconto = codiciPromozionali.includes(sconto)
+    console.log ("pcode" + codiceSconto)
+
+    let prezzo= 0
+    // SE 
+    // codicde inserito è presente nell'array sconti 
+    if (codiceSconto) {
+        // prezzofinale = risultato - 25%
+        console.log ("CODICE VALIDO")
+        prezzo = totalesenzasconti - totalesenzasconti * valoresconto
+        console.log ('prerzzo con sconto : ' + parseFloat(prezzo))
+
+        // ALTRIMENTI SE
+        //sconto è 0
+    }else if (sconto == "") {
+        // prezzofinale = risultato
+        prezzo = totalesenzasconti
+        console.log ('prezzo senza sconto : ' + parseFloat(prezzo))
+        // ALTRIMENTI
+    }else {
+        //ALERT + prezzofinale = risultato
+        alert("CODICE NON VALIDO!")
+        prezzo = totalesenzasconti
+    }
+
+    // rendere il prezzo finale a numero intero
+    prezzo = prezzo.toFixed(2)
+    const string = prezzo.toString()
+    const splitted = string.split('.')
+    const decimale = splitted [1]
+    const intero = splitted [0]
+    const stampaPrezzo = '<b>' + '&euro;' + intero + '</b>' + '<span style = "color:gray">' + ',' + decimale + '</span>'
+    //STAMPA IN HTML
+    document.getElementById('formatted-price').innerHTML = stampaPrezzo
+
+    
+}
+
 
